f1d868face7982fb4d32d0ed5dd1151647b4884e
Jonathan Göransson <jonathan.a.goransson@outlook.com>
2023-11-24 13:45:34 +0100

    addedd sql file

---
+++ api/db/sql.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const mysql = require("mysql2/promise");
+
+let db;
+const config = {
+    host: "database",
+    port: "3307",
+    user: "vteam",
+    password: "vteam",
+    database: "Elsparkcyklar",
+    multipleStatements: true,
+};
+
+(async function () {
+    db = await mysql.createConnection(config);
+
+    process.on("exit", () => {
+        db.end();
+    });
+})();
+
+const getAllBikes = async () => {
+    let sql = `CALL all_bikes();`;
+    let [rows] = await db.execute(sql);
+
+    console.log(rows[0]);
+
+    return rows[0];
+};
+
+module.exports = {
+    getAllBikes,
+};
