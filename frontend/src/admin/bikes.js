85d2e71a3748f7358df523b4c2431671bd9c40b6
Jonathan Göransson <jonathan.a.goransson@outlook.com>
2023-11-28 17:23:42 +0100

    updated test route to be used in docker

--- frontend/src/admin/bikes.js
+++ frontend/src/admin/bikes.js
@@ -4,9 +4,9 @@
 const apiKey = 1;
 
 const getBikes = async () => {
-    const response = await fetch(
-        `http://localhost:8080/api/bikes?apiKey=${apiKey}`
-    );
+    // Om man ska köra i Docker: http://api:8080/api/bikes?apiKey=1
+    // Om man ska köra localt så blir länken: http://localhost:8080/api/bikes?apiKey=1
+    const response = await fetch(`http://api:8080/api/bikes?apiKey=${apiKey}`);
     const result = await response.json();
 
     return result;
