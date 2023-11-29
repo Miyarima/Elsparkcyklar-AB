ff64dd0621df11500bf7e6f42e9450741816a277
A-Norre <alex.norre@outlook.com>
2023-11-29 11:21:23 +0100

    Added views for Admin

--- frontend/routes/admin.js
+++ frontend/routes/admin.js
@@ -13,4 +13,38 @@ router.get("/", (req, res) => {
     adminController.viewHome(req, res);
 });
 
+
+router.get("/adminlogin", (req, res) => {
+    res.render("admin_login.ejs");
+});
+
+router.get("/admin", (req, res) => {
+    res.render("front.ejs");
+});
+
+router.get("/cities", (req, res) => {
+    res.render("cities.ejs");
+});
+
+router.get("/customers", (req, res) => {
+    res.render("customers.ejs");
+});
+
+router.get("/bikes", (req, res) => {
+    res.render("bikes.ejs");
+});
+
+router.get("/stations", (req, res) => {
+    res.render("stations.ejs");
+});
+
+router.get("/parkingzones", (req, res) => {
+    res.render("parkingzones.ejs");
+});
+
+router.get("/overview", (req, res) => {
+    res.render("overview.ejs");
+});
+
+
 module.exports = router;
