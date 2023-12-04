13770c00c182e6321db0e65dd7f29a9402bb9803
A-Norre <alex.norre@outlook.com>
2023-12-04 11:47:03 +0100

    frontend:1.1

--- frontend/routes/user.js
+++ frontend/routes/user.js
@@ -13,4 +13,40 @@ router.get("/", (req, res) => {
     userController.viewHome(req, res);
 });
 
+router.get("/userlogin", (req, res) => {
+    res.render("user_login.ejs");
+});
+
+router.get("/createuser", (req, res) => {
+    res.render("create_user.ejs");
+});
+
+router.get("/user", (req, res) => {
+    res.render("front.ejs");
+});
+
+router.get("/details", (req, res) => {
+    res.render("details.ejs");
+});
+
+router.get("/detailsedit", (req, res) => {
+    res.render("details_edit.ejs");
+});
+
+router.get("/history", (req, res) => {
+    res.render("history.ejs");
+});
+
+router.get("/wallet", (req, res) => {
+    res.render("wallet.ejs");
+});
+
+router.get("/prepaid", (req, res) => {
+    res.render("wallet_prepaid.ejs");
+});
+
+router.get("/autogiro", (req, res) => {
+    res.render("wallet_autogiro.ejs");
+});
+
 module.exports = router;
