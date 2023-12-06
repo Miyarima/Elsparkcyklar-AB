109d2cfdc167195bea7b31d831c71fcb4e1ec20f
Olle <olle.frid2002@gmail.com>
2023-12-06 18:13:19 +0100

    documented code and added rows in invoice

--- api/db/functionsForBike.js
+++ api/db/functionsForBike.js
@@ -1,9 +1,15 @@
 "use strict";
-const { dbFunctions } = require("./databaseFunctions");
+const dbFunctions = require("./databaseFunctions");
 
 const dbFuncs = dbFunctions.gatheredDatabaseFunctions;
 
 const gatheredBikeFunctions = {
+    /**
+     * Gets all the bikes from the database
+     * @async
+     * @function
+     * @returns {Array.<Object>} An array containing objects as the result of the database query
+     */
     getAllBikes: async () => {
         const queryObject = {
             query: "SELECT * FROM `Bike`",
@@ -11,6 +17,13 @@ const gatheredBikeFunctions = {
         return await dbFuncs.promisifiedSimpleQueryFunc(queryObject);
     },
 
+    /**
+     * Function to get bike from bike id.
+     * @async
+     * @function
+     * @param {int} bikeId bike id
+     * @returns {Array.<Object>} An array containing objects as the result of the database query
+     */
     getOneBike: async (bikeId) => {
         const queryObject = {
             query: "SELECT * FROM `Bike` WHERE id = ?",
@@ -20,6 +33,13 @@ const gatheredBikeFunctions = {
         return await dbFuncs.promisifiedSimpleQueryFunc(queryObject);
     },
 
+    /**
+     * Function to delete the bike.
+     * @async
+     * @function
+     * @param {int} bikeId bike id.
+     * @returns {Array.<Object>} An array containing objects as the result of the database query
+     */
     deleteBike: async (bikeId) => {
         const queryObject = {
             query: "DELETE FROM Bike WHERE id = ?",
@@ -29,6 +49,13 @@ const gatheredBikeFunctions = {
         return await dbFuncs.promisifiedSimpleQueryFunc(queryObject);
     },
 
+    /**
+     * Function to get bike speed and the max speed in its zone
+     * @async
+     * @function
+     * @param {int} bikeId bike id.
+     * @returns {Array.<Object>} An array containing objects as the result of the database query
+     */
     getMaxSpeedForBike: async (bikeId) => {
         const queryObject = {
             query: "SELECT b.speed AS `speed`, z.id AS `zone_id`, z.max_speed AS `max_speed_in_zone` FROM Bike b LEFT JOIN `Zone` z ON b.zone_id = z.id WHERE b.id = ?",
@@ -37,6 +64,13 @@ const gatheredBikeFunctions = {
         return await dbFuncs.promisifiedSimpleQueryFunc(queryObject);
     },
 
+    /**
+     * Function to all bikes and its power status
+     * @async
+     * @function
+     * @param {string} status Status string
+     * @returns {Array.<Object>} An array containing objects as the result of the database query
+     */
     selectBikesFromPowerStatus: async (status) => {
         const queryObject = {
             query: "SELECT id,power FROM Bike WHERE power = ?",
@@ -45,6 +79,14 @@ const gatheredBikeFunctions = {
         return await dbFuncs.promisifiedSimpleQueryFunc(queryObject);
     },
 
+    /**
+     * Function to all bikes and its power status
+     * @async
+     * @function
+     * @param {int} bikeId bikeId
+     * @param {string} status Status string
+     * @returns {Array.<Object>} An array containing objects as the result of the database query
+     */
     changePowerStatus: async (bikeId, status) => {
         const queryObject = {
             query: "UPDATE Bike SET power = ? WHERE id = ?",
@@ -53,6 +95,14 @@ const gatheredBikeFunctions = {
         return await dbFuncs.promisifiedSimpleQueryFunc(queryObject);
     },
 
+    /**
+     * Function to all bikes and its power status
+     * @async
+     * @function
+     * @param {int} bikeId bikeId
+     * @param {int} userId Status string
+     * @returns {bool} True if the querys were succesful, otherwise an error.
+     */
     unlockBike: async (bikeId, userId) => {
         const queryObject = {
             query: "INSERT INTO Travel(bike_id, `username`, `status`, start_longitude, start_latitude) VALUES (?, ?, 'Ongoing', (SELECT longitude FROM Bike WHERE id = ?),(SELECT latitude FROM Bike WHERE id = ?))",
@@ -70,6 +120,13 @@ const gatheredBikeFunctions = {
         ]);
     },
 
+    /**
+     * Function to all bikes and its power status
+     * @async
+     * @function
+     * @param {int} bikeId bikeId
+     * @returns {bool} True if the querys were succesful, otherwise an error.
+     */
     lockBike: async (bikeId) => {
         const currentDate = new Date();
 
@@ -77,8 +134,12 @@ const gatheredBikeFunctions = {
         futureDate.setDate(currentDate.getDate() + 30);
 
         const queryObject = {
-            query: `INSERT INTO Invoice(travel_id, issue_date, expiry_date) VALUES ((SELECT id FROM Travel WHERE status = 'Ongoing' AND bike_id = ?), '${currentDate.toDateString()}', '${futureDate.toDateString()}')`,
-            params: [bikeId],
+            query: "INSERT INTO Invoice(travel_id, issue_date, expiry_date) VALUES ((SELECT id FROM Travel WHERE status = 'Ongoing' AND bike_id = ?), ?, ?)",
+            params: [
+                bikeId,
+                currentDate.toDateString(),
+                futureDate.toDateString(),
+            ],
         };
 
         const queryObject2 = {
--- api/db/functionsForCity.js
+++ api/db/functionsForCity.js
@@ -1,9 +1,15 @@
 "use strict";
-const { dbFunctions } = require("./databaseFunctions");
+const dbFunctions = require("./databaseFunctions");
 
 const dbFuncs = dbFunctions.gatheredDatabaseFunctions;
 
 const gatheredCityFunctions = {
+    /**
+     * Selects all cities
+     * @async
+     * @function
+     * @returns {Array.<Object>} An array containing objects as the result of the database query.
+     */
     selectAllCities: async () => {
         const queryObject = {
             query: "SELECT * FROM City",
@@ -11,6 +17,13 @@ const gatheredCityFunctions = {
         return await dbFuncs.promisifiedSimpleQueryFunc(queryObject);
     },
 
+    /**
+     * Selects available ports for station
+     * @async
+     * @function
+     * @param {int} stationId stationId
+     * @returns {Array.<Object>} An array containing objects as the result of the database query.
+     */
     availablePortsForStation: async (stationId) => {
         const queryObject = {
             query: "SELECT * FROM Port WHERE station_id = ?",
@@ -19,6 +32,13 @@ const gatheredCityFunctions = {
         return await dbFuncs.promisifiedSimpleQueryFunc(queryObject);
     },
 
+    /**
+     * Selects specific city.
+     * @async
+     * @function
+     * @param {int} cityId cityId
+     * @returns {Array.<Object>} An array containing objects as the result of the database query.
+     */
     specificCity: async (cityId) => {
         const queryObject = {
             query: "SELECT * FROM City WHERE id = ?",
@@ -27,6 +47,12 @@ const gatheredCityFunctions = {
         return await dbFuncs.promisifiedSimpleQueryFunc(queryObject);
     },
 
+    /**
+     * All bikes and the city it belongs to
+     * @async
+     * @function
+     * @returns {Array.<Object>} An array containing objects as the result of the database query.
+     */
     allBikesAndItsCities: async () => {
         const queryObject = {
             query: "SELECT b.*, c.name FROM Bike b LEFT JOIN City c ON b.id = c.id",
@@ -34,6 +60,12 @@ const gatheredCityFunctions = {
         return await dbFuncs.promisifiedSimpleQueryFunc(queryObject);
     },
 
+    /**
+     * All bikes and the city it belongs to
+     * @async
+     * @function
+     * @returns {Array.<Object>} An array containing objects as the result of the database query.
+     */
     bikesAttachedToStation: async () => {
         const queryObject = {
             query: "SELECT b.id, b.charging, b.battery, b.status, s.id, s.`address`,p.id FROM Bike b RIGHT JOIN Port p ON b.id = p.bike_id LEFT JOIN Station s ON p.station_id = s.id",
