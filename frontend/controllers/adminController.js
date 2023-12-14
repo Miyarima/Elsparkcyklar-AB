3f27bf3dafeb20581ac487fd6b2c1bccf83f8381
sucro susm92 <susm20@student.bth.se>
2023-12-14 16:44:33 +0100

    added read functionality for admin/bikes & admin/customers

--- frontend/controllers/adminController.js
+++ frontend/controllers/adminController.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const bikes = require("../src/admin/bikes.js");
+const baseURL = "http://api:8080/api";
 
 const viewHome = async (req, res) => {
     const allBikes = await bikes.getBikes();
@@ -14,6 +15,58 @@ const viewHome = async (req, res) => {
     });
 };
 
+const allCities = async(req, res, apiKey) => {
+    try {
+        const response = await fetch(`${baseURL}/cities?apiKey=${apiKey}`);
+        const cityData = await response.json();
+        const cityMalmoe = [];
+        const cityStockholm = [];
+
+        for (const stad of cityData.cities) {
+            if (stad.city === 'Stockholm') {
+                cityStockholm.push(stad);
+            } else {
+                cityMalmoe.push(stad);
+            }
+        }
+
+        console.log(cityMalmoe);
+        console.log(cityMalmoe)
+
+        res.render("cities.ejs");
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+const allBikes = async(req, res, apiKey) => {
+    try {
+        const response = await fetch(`${baseURL}/bikes?apiKey=${apiKey}`);
+        const bikeData = await response.json();
+
+        res.render("bikes.ejs", { bikes: bikeData.bikes });
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
+const allCustomers = async(req, res, apiKey) => {
+    try {
+        const response = await fetch(`${baseURL}/users?apiKey=${apiKey}`);
+        const userData = await response.json();
+
+        res.render("customers.ejs", { users: userData.users });
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
 module.exports = {
     viewHome,
+    allCities,
+    allBikes,
+    allCustomers,
 };
