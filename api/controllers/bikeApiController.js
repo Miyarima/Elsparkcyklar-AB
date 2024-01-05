ca254c9496da6aa1ebae9ba9ac7537883462c654
Jonathan Göransson <jonathan.a.goransson@outlook.com>
2024-01-05 13:58:20 +0100

    added try catch on rent and return

--- api/controllers/bikeApiController.js
+++ api/controllers/bikeApiController.js
@@ -71,38 +71,44 @@ const rentBike = async (req, res) => {
     const userId = req.params.userid;
     const contentType = req.headers["content-type"];
 
-    // Check for apiKey
-    if (!apiKey) {
-        return res.status(403).json({ error: "Please provide an API key." });
+    try {
+        // Check for apiKey
+        if (!apiKey) {
+            return res
+                .status(403)
+                .json({ error: "Please provide an API key." });
+        }
+
+        // Check for the required headers
+        if (!contentType || contentType !== "application/json") {
+            return res
+                .status(400)
+                .json({ error: "Content-Type must be application/json" });
+        }
+
+        // checking if a user was provided in the body
+        if (!userId) {
+            return res
+                .status(403)
+                .json({ error: "No user to rent the bike was provided" });
+        }
+
+        // If the provided bikeId didn't exsist in the database
+        if (!bikeId) {
+            return res
+                .status(403)
+                .json({ error: "Please provide correct ID for a bike." });
+        }
+
+        const rent = await db.gatheredBikeFunctions.unlockBike(bikeId, userId);
+
+        return res.status(200).json({
+            message: "bike has been rented",
+            status: rent,
+        });
+    } catch (error) {
+        return res.status(500).json({ error: `${error}` });
     }
-
-    // Check for the required headers
-    if (!contentType || contentType !== "application/json") {
-        return res
-            .status(400)
-            .json({ error: "Content-Type must be application/json" });
-    }
-
-    // checking if a user was provided in the body
-    if (!userId) {
-        return res
-            .status(403)
-            .json({ error: "No user to rent the bike was provided" });
-    }
-
-    // If the provided bikeId didn't exsist in the database
-    if (!bikeId) {
-        return res
-            .status(403)
-            .json({ error: "Please provide correct ID for a bike." });
-    }
-
-    const rent = await db.gatheredBikeFunctions.unlockBike(bikeId, userId);
-
-    return res.status(200).json({
-        message: "bike has been rented",
-        status: rent,
-    });
 };
 
 const returnBike = async (req, res) => {
@@ -111,41 +117,47 @@ const returnBike = async (req, res) => {
     const longitude = req.params.longitude;
     const latitude = req.params.latitude;
 
-    if (!apiKey) {
-        return res.status(403).json({ error: "Please provide an API key." });
-    }
+    try {
+        if (!apiKey) {
+            return res
+                .status(403)
+                .json({ error: "Please provide an API key." });
+        }
 
-    if (!bikeId) {
-        console.log("NO BIKE ID GIVEN!");
-        return res
-            .status(403)
-            .json({ error: "Please provide correct ID for a bike." });
-    }
+        if (!bikeId) {
+            console.log("NO BIKE ID GIVEN!");
+            return res
+                .status(403)
+                .json({ error: "Please provide correct ID for a bike." });
+        }
 
-    if (!longitude) {
-        console.log("No coordinates for Longitude given!");
-        return res.status(403).json({
-            error: "Please provide a coordinate for longitude for a bike.",
+        if (!longitude) {
+            console.log("No coordinates for Longitude given!");
+            return res.status(403).json({
+                error: "Please provide a coordinate for longitude for a bike.",
+            });
+        }
+
+        if (!latitude) {
+            console.log("No coordinates for Latitude given!");
+            return res.status(403).json({
+                error: "Please provide a coordinate for latitude for a bike.",
+            });
+        }
+
+        const bike = await db.gatheredBikeFunctions.lockBike(
+            bikeId,
+            longitude,
+            latitude,
+        );
+
+        return res.status(200).json({
+            message: "bike has been returned",
+            status: bike,
         });
+    } catch (error) {
+        return res.status(500).json({ error: `${error}` });
     }
-
-    if (!latitude) {
-        console.log("No coordinates for Latitude given!");
-        return res.status(403).json({
-            error: "Please provide a coordinate for latitude for a bike.",
-        });
-    }
-
-    const bike = await db.gatheredBikeFunctions.lockBike(
-        bikeId,
-        longitude,
-        latitude,
-    );
-
-    return res.status(200).json({
-        message: "bike has been returned",
-        status: bike,
-    });
 };
 
 const getBikePosition = async (req, res) => {
